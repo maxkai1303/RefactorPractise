7acdcfa3d9e5ea0ee8ee40ff9788d0aa9f4573b0
Max <k1071303@gmail.com>
2022-11-27 11:10:15 +0800

    Create itemViewModel in the right context to improve the design and eliminate fatal errors required by untyped models

--- iACC/ItemViewModel.swift
+++ iACC/ItemViewModel.swift
@@ -7,4 +7,52 @@ import Foundation
 struct ItemViewModel {
     let title: String
     let subtitle: String
+    //Step2.1 我們在這裡加一個 closure 讓外面的人提供這段邏輯給我
+    let select: () -> Void
+    
+    init(_ item: Any, longDateStyle: Bool, selection: @escaping () -> Void) {
+        if let friend = item as? Friend {
+            self.init(friend: friend, selection: selection)
+        } else if let card = item as? Card {
+            self.init(card: card, selection: selection)
+        } else if let transfer = item as? Transfer {
+            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
+        } else {
+            fatalError("unknown item: \(item)")
+        }
+    }
+    // Step1.2 移除 Any type
+    init(friend: Friend, selection: @escaping () -> Void) {
+        title = friend.name
+        subtitle = friend.phone
+        //Step2.2 每個類型都有自己的選擇邏輯由外面的人注入
+        select = selection
+    }
+    
+    init(card: Card, selection: @escaping () -> Void) {
+        title = card.number
+        subtitle = card.holder
+        select = selection
+    }
+    
+    init(transfer: Transfer, longDateStyle: Bool, selection: @escaping () -> Void) {
+        let numberFormatter = Formatters.number
+        numberFormatter.numberStyle = .currency
+        numberFormatter.currencyCode = transfer.currencyCode
+        
+        let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
+        title = "\(amount) • \(transfer.description)"
+        
+        let dateFormatter = Formatters.date
+        if longDateStyle {
+            dateFormatter.dateStyle = .long
+            dateFormatter.timeStyle = .short
+            subtitle = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
+        } else {
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            subtitle = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
+        }
+        select = selection
+    }
 }
--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -5,7 +5,8 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-	var items = [Any]()
+    // Step4 把詐騙的 Any 換成我們自己的 Type
+	var items = [ItemViewModel]()
 	
 	var retryCount = 0
 	var maxRetryCount = 0
@@ -70,44 +71,83 @@ class ListViewController: UITableViewController {
 		if fromFriendsScreen {
 			FriendsAPI.shared.loadFriends { [weak self] result in
 				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
+                    // Step5.1 這裡取得 result 的的部分就會開始哭，一樣用 map 塞他的嘴，而且我們已知這裡是朋友，就直接用朋友轉
+                    self?.handleAPIResult(result.map { items in
+                        // Step5.2 把本來在 handleAPIResult 的是否捧油判斷整組搬來，這樣又省了一個 bool
+                        // 而且也不用 as! [Friend]
+                        if User.shared?.isPremium == true {
+                            (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items)
+                        }
+                        return items.map { item in
+                            ItemViewModel(friend: item, selection: {
+                                self?.select(friend: item)
+                            })
+                        }
+                    })
 				}
 			}
 		} else if fromCardsScreen {
 			CardAPI.shared.loadCards { [weak self] result in
 				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
+					self?.handleAPIResult(result.map { items in
+                        items.map { item in
+                            ItemViewModel(card: item, selection: {
+                                self?.select(card: item)
+                            })
+                        }
+                    })
 				}
 			}
 		} else if fromSentTransfersScreen || fromReceivedTransfersScreen {
-			TransfersAPI.shared.loadTransfers { [weak self] result in
-				DispatchQueue.mainAsyncIfNeeded {
-					self?.handleAPIResult(result)
-				}
-			}
-		} else {
+            TransfersAPI.shared.loadTransfers { [weak self, longDateStyle, fromSentTransfersScreen] result in
+                DispatchQueue.mainAsyncIfNeeded {
+                    self?.handleAPIResult(result.map { items in
+                        // Step5.3 一樣把 handleAPIResult 關於 transfers 的東西搬來，就不用再去檢查 type
+//                        if fromSentTransfersScreen {
+//                            filteredItems = filteredItems.filter(\.isSender)
+//                        } else {
+//                            filteredItems = filteredItems.filter { !$0.isSender }
+//                        }
+                        // 把上面的 filter 移到這裡處理
+                        items
+                            .filter { fromSentTransfersScreen ? $0.isSender : !$0.isSender }
+                            .map { item in
+                            ItemViewModel(transfer: item,
+                                          longDateStyle: longDateStyle,
+                                          selection: {
+                                self?.select(transfer: item)
+                            })
+                        }
+                    })
+                }
+            }
+        } else {
 			fatalError("unknown context")
 		}
 	}
-	
-	private func handleAPIResult<T>(_ result: Result<[T], Error>) {
+	// Step5 不要吃泛形
+	private func handleAPIResult(_ result: Result<[ItemViewModel], Error>) {
 		switch result {
 		case let .success(items):
-			if fromFriendsScreen && User.shared?.isPremium == true {
-				(UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items as! [Friend])
-			}
 			self.retryCount = 0
 			
-			var filteredItems = items as [Any]
-			if let transfers = items as? [Transfer] {
-				if fromSentTransfersScreen {
-					filteredItems = transfers.filter(\.isSender)
-				} else {
-					filteredItems = transfers.filter { !$0.isSender }
-				}
-			}
-			
-			self.items = filteredItems
+            self.items = items
+            // Step5.4 結果這邊就不需要再判斷了
+//            self.items = filteredItems.map({ item in
+//                // Step4.1 在這裡本來 filteredItems 是拿到 Any(102行) 我們改了型別後，他會在那邊哭，
+//                // 所以我們在這邊 map 他，轉成我們想要的形狀
+//                ItemViewModel(item, longDateStyle: longDateStyle, selection: { [weak self] in
+//                    if let friend = item as? Friend {
+//                        self?.select(friend: friend)
+//                    } else if let card = item as? Card {
+//                        self?.select(card: card)
+//                    } else if let transfer = item as? Transfer {
+//                        self?.select(transfer: transfer)
+//                    } else {
+//                        fatalError("unknown item: \(item)")
+//                    }
+//                })
+//            })
 			self.refreshControl?.endRefreshing()
 			self.tableView.reloadData()
 			
@@ -126,21 +166,22 @@ class ListViewController: UITableViewController {
 					DispatchQueue.mainAsyncIfNeeded {
 						switch result {
 						case let .success(items):
-							self?.items = items
+                            // Step4.1.2 這裡也有一個會在那邊哭，但是這邊很明顯是拿 friend cache 所以我們直接轉朋友
+                            self?.items = items.map({ item in
+                                ItemViewModel(friend: item, selection: { [weak self] in
+                                    self?.select(friend: item)
+                                })
+                            })
 							self?.tableView.reloadData()
 							
 						case let .failure(error):
-							let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-							alert.addAction(UIAlertAction(title: "Ok", style: .default))
-							self?.presenterVC.present(alert, animated: true)
+                            self?.showError(error: error)
 						}
 						self?.refreshControl?.endRefreshing()
 					}
 				}
 			} else {
-				let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-				alert.addAction(UIAlertAction(title: "Ok", style: .default))
-				self.presenterVC.present(alert, animated: true)
+                self.showError(error: error)
 				self.refreshControl?.endRefreshing()
 			}
 		}
@@ -157,74 +198,67 @@ class ListViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let item = items[indexPath.row]
 		let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "ItemCell")
-		cell.configure(item, longDateStyle: longDateStyle)
+		cell.configure(item)
 		return cell
 	}
-	
+	// Step3 移除這邊的 item 類型判斷，把東西移到我知道他是什麼 Type 的時候去判斷（在 ItemViewModel)
+    // Step4.2 在上面取回資料的時候就先判斷，讓 item 是正確的型別就可以把判斷都拿掉了
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		let item = items[indexPath.row]
-		if let friend = item as? Friend {
-			let vc = FriendDetailsViewController()
-			vc.friend = friend
-			navigationController?.pushViewController(vc, animated: true)
-		} else if let card = item as? Card {
-			let vc = CardDetailsViewController()
-			vc.card = card
-			navigationController?.pushViewController(vc, animated: true)
-		} else if let transfer = item as? Transfer {
-			let vc = TransferDetailsViewController()
-			vc.transfer = transfer
-			navigationController?.pushViewController(vc, animated: true)
-		} else {
-			fatalError("unknown item: \(item)")
-		}
-	}
-	
-	@objc func addCard() {
-		navigationController?.pushViewController(AddCardViewController(), animated: true)
-	}
-	
-	@objc func addFriend() {
-		navigationController?.pushViewController(AddFriendViewController(), animated: true)
-	}
-	
-	@objc func sendMoney() {
-		navigationController?.pushViewController(SendMoneyViewController(), animated: true)
-	}
-	
-	@objc func requestMoney() {
-		navigationController?.pushViewController(RequestMoneyViewController(), animated: true)
+        item.select()
 	}
 }
 
+// Step1 讓 Cell 根本不需要判斷直接收要顯示什麼內容，把內容搬去 ItemViewModel init
 extension UITableViewCell {
-	func configure(_ item: Any, longDateStyle: Bool) {
-		if let friend = item as? Friend {
-			textLabel?.text = friend.name
-			detailTextLabel?.text = friend.phone
-		} else if let card = item as? Card {
-			textLabel?.text = card.number
-			detailTextLabel?.text = card.holder
-		} else if let transfer = item as? Transfer {
-			let numberFormatter = Formatters.number
-			numberFormatter.numberStyle = .currency
-			numberFormatter.currencyCode = transfer.currencyCode
-			
-			let amount = numberFormatter.string(from: transfer.amount as NSNumber)!
-			textLabel?.text = "\(amount) • \(transfer.description)"
-			
-			let dateFormatter = Formatters.date
-			if longDateStyle {
-				dateFormatter.dateStyle = .long
-				dateFormatter.timeStyle = .short
-				detailTextLabel?.text = "Sent to: \(transfer.recipient) on \(dateFormatter.string(from: transfer.date))"
-			} else {
-				dateFormatter.dateStyle = .short
-				dateFormatter.timeStyle = .short
-				detailTextLabel?.text = "Received from: \(transfer.sender) on \(dateFormatter.string(from: transfer.date))"
-			}
-		} else {
-			fatalError("unknown item: \(item)")
-		}
-	}
+	func configure(_ vm: ItemViewModel) {
+        textLabel?.text = vm.title
+        detailTextLabel?.text = vm.subtitle
+    }
+}
+// Step2 把選擇不同 Type 的部分拆開來個別呼叫
+extension UIViewController {
+    func select(friend: Friend) {
+        let vc = FriendDetailsViewController()
+        vc.friend = friend
+        //        我們需要跟 navigationControler 解耦所以改用 show
+//        navigationController?.pushViewController(vc, animated: true)
+        show(vc, sender: self)
+    }
+    
+    func select(card: Card) {
+        let vc = CardDetailsViewController()
+        vc.card = card
+        show(vc, sender: self)
+    }
+    
+    func select(transfer: Transfer) {
+        let vc = TransferDetailsViewController()
+        vc.transfer = transfer
+        show(vc, sender: self)
+    }
+    
+    @objc func addCard() {
+        show(AddCardViewController(), sender: self)
+    }
+    
+    @objc func addFriend() {
+        show(AddFriendViewController(), sender: self)
+    }
+    
+    @objc func sendMoney() {
+        show(SendMoneyViewController(), sender: self)
+    }
+    
+    @objc func requestMoney() {
+        show(RequestMoneyViewController(), sender: self)
+    }
+    
+    // 將重複行為另外獨立出來，使用爸爸給你的好接口脫離 presentation
+    func showError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+//                self.presenterVC.present(alert, animated: true)
+        self.showDetailViewController(alert, sender: self)
+    }
 }
